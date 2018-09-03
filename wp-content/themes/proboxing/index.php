8b85c0b5d7afd6a2c08d8205ca9bfc9e035037a4
smelgarejo <scarlett.melgarejo@gmail.com>
2018-09-03 17:30:05 -0300

    modificación index.php del theme

--- wp-content/themes/proboxing/index.php
+++ wp-content/themes/proboxing/index.php
@@ -38,100 +38,6 @@ get_header() ?>
 </div>
 <!--========== END SWIPER SLIDER ==========-->
 
-<!--========== PAGE CONTENT ==========-->
-<!-- Features -->
-<div id="js__scroll-to-section" class="container g-padding-y-80--xs g-padding-y-125--sm">
-    <div class="g-text-center--xs g-margin-b-100--xs">
-        <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Welcome to Megakit</p>
-        <h2 class="g-font-size-32--xs g-font-size-36--md">We Create Beautiful Experiences <br> That Drive Successful Businesses.</h2>
-    </div>
-    <div class="row g-margin-b-60--xs g-margin-b-70--md">
-        <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".1s">
-                        <i class="g-font-size-28--xs g-color--primary ti-desktop"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Responsive Layout</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".2s">
-                        <i class="g-font-size-28--xs g-color--primary ti-settings"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Fully Customizable</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".3s">
-                        <i class="g-font-size-28--xs g-color--primary ti-ruler-alt-2"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Pixel Perfect</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- // end row  -->
-    <div class="row">
-        <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".4s">
-                        <i class="g-font-size-28--xs g-color--primary ti-package"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Endless Possibilities</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4 g-margin-b-60--xs g-margin-b-0--md">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".5s">
-                        <i class="g-font-size-28--xs g-color--primary ti-star"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Powerful Performance</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="clearfix">
-                <div class="g-media g-width-30--xs">
-                    <div class="wow fadeInDown" data-wow-duration=".3" data-wow-delay=".6s">
-                        <i class="g-font-size-28--xs g-color--primary ti-panel"></i>
-                    </div>
-                </div>
-                <div class="g-media__body g-padding-x-20--xs">
-                    <h3 class="g-font-size-18--xs">Parallax Support</h3>
-                    <p class="g-margin-b-0--xs">This is where we sit down, grab a cup of coffee and dial in the details.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- // end row  -->
-</div>
-<!-- End Features -->
-
 <!-- Parallax -->
 <div class="js__parallax-window" style="background: url(<?php echo $theme_url ?>/assets/images/1920x1080/03.jpg) 50% 0 no-repeat fixed;">
     <div class="container g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
@@ -372,7 +278,7 @@ get_header() ?>
                 <img class="img-responsive" src="<?php echo $theme_url ?>/assets/images/970x970/01.jpg" alt="Image">
                 <div class="g-bg-color--white g-box-shadow__dark-lightest-v2 g-text-center--xs g-padding-x-40--xs g-padding-y-40--xs">
                     <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">Promociones</p>
-                    <h3 class="g-font-size-22--xs g-letter-spacing--1"><a href="http://keenthemes.com/">Create Something Great.</a></h3>
+                    <h3 class="g-font-size-22--xs g-letter-spacing--1"><a href="http://keenthemes.com/">Ap</a></h3>
                     <p>The time has come to bring those ideas and plans to life.</p>
                 </div>
             </article>
