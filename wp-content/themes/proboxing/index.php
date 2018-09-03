ef4b54721c5ec4d527703f773d2a07951aafc52e
smelgarejo <scarlett.melgarejo@gmail.com>
2018-09-03 15:12:44 -0300

    info promociones

--- wp-content/themes/proboxing/index.php
+++ wp-content/themes/proboxing/index.php
@@ -136,9 +136,9 @@ get_header() ?>
 <div class="js__parallax-window" style="background: url(<?php echo $theme_url ?>/assets/images/1920x1080/03.jpg) 50% 0 no-repeat fixed;">
     <div class="container g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
         <div class="g-margin-b-80--xs">
-            <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white">The Fastest Way To Develop</h2>
+            <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white">Conoce nuestras promociones</h2>
         </div>
-        <a href="http://keenthemes.com/" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50">Learn More</a>
+        <a href="#" class="text-uppercase s-btn s-btn--md s-btn--white-brd g-radius--50">ver más</a>
     </div>
 </div>
 <!-- End Parallax -->
